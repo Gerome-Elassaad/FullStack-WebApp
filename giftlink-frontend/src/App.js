1c1a8a2e1c4c5699205ad56c7848672adf5a7e53
Gerome-Elassaad <repo-booster@proton.me>
2024-12-04 04:33:07 +1100

    updates

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar'; // Import Navbar
+import MainPage from './components/MainPage/MainPage'; // Import MainPage
+import DetailsPage from './components/DetailsPage/DetailsPage'; // Import DetailsPage
+import LoginPage from './components/LoginPage/LoginPage'; // Import LoginPage
+import Profile from './components/Profile/Profile'; // Import Profile
+import RegisterPage from './components/RegisterPage/RegisterPage'; // Import RegisterPage
+import SearchPage from './components/SearchPage/SearchPage'; // Import SearchPage
+import './App.css'; // Global styles for App
 
 function App() {
-
   return (
-    <>
-        <Navbar/>
-        <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-        </Routes>
-        </>
+    <Router>
+      <Navbar /> {/* Navbar on all pages */}
+      <Routes>
+        {/* Define Routes for each page */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home.html" element={<MainPage />} /> {/* Alternative home route */}
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/gifts/:productId" element={<DetailsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </Router>
   );
 }
 
